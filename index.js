1fdffd04d78db151938cc61df7599bac4fa3c1dc
Lucas Nascimento <csalucasnascimento@gmail.com>
2018-05-04 12:16:46 +1000

    update

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function toggle(x) {
     for (var i = 0; i < arguments.length; i++)
         document.getElementsByClassName(arguments[i])[0].classList.toggle('active')
 }
+
+function toggleInactive(x) {
+    for (var i = 0; i < arguments.length; i++)
+        document.getElementsByClassName(arguments[i])[0].classList.toggle('inactive')
+}
